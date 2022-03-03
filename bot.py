594c44a979d8f6ae97077bca1560e2dd61ad13ea
Ben Wu <benayaj.2004@gmail.com>
2022-03-03 18:56:28 +0700

    add command handling roll command

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def create_client(bot: Hikari.GatewayBot) -> Tanjun.Client:
-    client = (Tanjun.Client.from_gateway_bot(bot, mention_prefix=True).add_prefix('0_'))
+    client = (Tanjun.Client.from_gateway_bot(bot, mention_prefix=True, declare_global_commands=775253878312009768).add_prefix('0_'))
+    client.load_modules('command.roll')
     return client
 
 def create_bot() -> Hikari.GatewayBot:
---
+++ command/roll.py
@@ -0,0 +1,25 @@
+import random
+import tanjun as Tanjun
+
+async def roll(ctx: Tanjun.abc.SlashContext, dice: str) -> None:
+    n = dice.find('d')
+    d_amount = dice[:n]
+    d_face = dice[n:]
+
+    rolls = []
+
+    for _ in range(d_amount):
+        roll = random.randint(0, d_face)
+        rolls.append(roll)
+
+    await ctx.respond(f"{rolls}")
+
+roll_cmd = Tanjun.SlashCommand(
+    roll,
+    'roll',
+    'Rolls the dice. Defaults to 1d20.',
+)
+
+roll_cmd.add_str_option('dice', 'The type of dice to roll.', default='1d20')
+
+component = Tanjun.Component().load_from_scope().make_loader()

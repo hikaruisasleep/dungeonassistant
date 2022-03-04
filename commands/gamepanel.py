77fb2cf076da42dcb37256b2b105d1b161f610e3
Ben Wu <benayaj.2004@gmail.com>
2022-03-05 02:20:48 +0700

    still doesnt work

--- commands/gamepanel.py
+++ commands/gamepanel.py
@@ -1,41 +1,32 @@
-import asyncio
-from datetime import datetime
 import hikari as Hikari
 import tanjun as Tanjun
 import yuyo as Yuyo
 
-async def campaign_manager(ctx: Tanjun.abc.Context, component_client: Yuyo.ComponentClient = Tanjun.injected(type=Yuyo.ComponentClient)) -> None:
+component = Tanjun.Component()
+
+async def persistence_callback(ctx: Yuyo.ComponentContext):
+    await ctx.respond(f'Pressed.')
+
+@component.with_listener(Hikari.StartedEvent)
+async def on_bot_ready(_: Hikari.StartedEvent, component_client: Yuyo.ComponentClient = Tanjun.injected(type=Yuyo.ComponentClient)):
+    component_client.set_constant_id('primary-click', persistence_callback)
+
+@component.with_command
+@Tanjun.as_slash_command('campaign', 'Global campaign manager panel')
+async def campaign_manager(ctx: Tanjun.abc.Context) -> None:
     row = (
         ctx.rest.build_action_row()
-        .add_button(Hikari.ButtonStyle.PRIMARY, 'primary_button')
+        .add_button(Hikari.ButtonStyle.PRIMARY, 'primary-click')
         .set_label("Primary button")
         .add_to_container()
-        .add_button(Hikari.ButtonStyle.SECONDARY, 'secondary_button')
-        .set_label("Secondary button")
-        .add_to_container()
     )
 
-    message = await ctx.respond(
+    await ctx.respond(
         "Yuyo component test",
         component=row,
-        ensure_result=True
+        ensure_result=True,
     )
 
-    executor = Yuyo.components.WaitFor(authors=(ctx.author.id,), timeout=datetime.timedelta(seconds=30))
-    component_client.set_executor(message.id, executor)
-
-    try:
-        result = await executor.wait_for()
-        msg_id = result.interaction.custom_id
-    except asyncio.TimeoutError:
-        await ctx.respond("Timeout 30000ms")
-    else:
-        await result.respond(f"Interaction ID: {msg_id}")
-
-campaign_cmd = Tanjun.SlashCommand(
-    campaign_manager,
-    'campaign',
-    'a',
-)
-
-component = Tanjun.Component().load_from_scope().make_loader()
+@Tanjun.as_loader
+def load(client: Tanjun.abc.Client) -> None:
+    client.add_component(component.copy())
